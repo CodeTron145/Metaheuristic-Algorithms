011eb0b095117b4772bd2b30975ff1a959d965ed
CodeTron145 <batmail780@gmail.com>
2022-03-19 19:21:09 +0100

    repair bug in line 29(object copy problem)

--- l1-Heuristics/2opt/2opt.py
+++ l1-Heuristics/2opt/2opt.py
@@ -1,3 +1,4 @@
+from curses.panel import new_panel
 import tsplib95 as tsp
 import numpy as np
 
@@ -25,13 +26,17 @@ def find_best_distance(tsp_file, existing_route):
 
     for i in range(1, n):
         for k in range(i+1, n):
-            new_route = two_opt(existing_route, i, k)
+            new_route = two_opt(existing_route[:], i, k)
             new_distance = calc_distance(new_route, weight_matrix)
             
             if new_distance < best_distance:
                 existing_route = new_route
                 best_distance = new_distance
-                i = n
                 break
 
     return best_distance
+
+
+l = [1, 22, 49, 32, 36, 35, 34, 39, 40, 38, 37, 48, 24, 5, 15, 6, 4, 25, 46, 44, 16, 50, 20, 23, 31, 18, 3, 19, 45, 41, 8, 10, 9, 43, 33, 51, 12, 28, 27, 26, 47, 13, 14, 52, 11, 29, 30, 21, 17, 42, 7, 2, 1]
+problem = find_best_distance('templates/berlin52.tsp', l)
+print(problem)
